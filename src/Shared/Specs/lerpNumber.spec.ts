e15a8026bd838ee6518094855bfe3c48a33e564a
memothelemo <31132624+memothelemo@users.noreply.github.com>
2021-09-04 05:13:08 +0800

    Extended test for lerpNumber

--- src/Shared/Specs/lerpNumber.spec.ts
+++ src/Shared/Specs/lerpNumber.spec.ts
@@ -6,4 +6,8 @@ export = (): void => {
 	it("should be equal to 0.5 from range of 0 -> 1 in an alpha of 0.5", () => {
 		expect(lerpNumber(0, 1, 0.5)).to.equal(0.5);
 	});
+
+	it("should be equal to 40 from range of 50 -> 10 in an alpha of 0.25", () => {
+		expect(lerpNumber(50, 10, 0.25)).to.equal(40);
+	});
 };
